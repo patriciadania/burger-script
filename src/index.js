9889b000ab5651ba3e449afb067e2d769afa0a2c
Thainara Tabile <thainara.tabilebenevides@gmail.com>
2023-05-06 20:15:40 -0300

    config-btn-acessar. Co-authored-by: patriciadania patricia.adania@hotmail.com

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AppRoutes from './routes';
+import AppRoutes from './Routes';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
