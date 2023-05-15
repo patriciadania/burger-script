3f0ebe23f72741ce094c33b2a614f583a0063c03
Thainara Tabile <thainara.tabilebenevides@gmail.com>
2023-05-15 14:03:55 -0300

    implementa função que exibe os pedidos Co-authored-by: patriciadania patricia.adania@hotmail.com

--- src/API/Pedidos.js
+++ src/API/Pedidos.js
@@ -1,27 +1,48 @@
-import Order from './models/Order';
+//import Order from './models/Order';
 
-const API_URL = 'http://localhost:8080';
+// const API_URL = 'http://localhost:8080';
 
-const getOrders = async () => {
-  const orders = await Order.find();
-  return orders;
-}
+// const getOrders = async () => {
+//   const orders = await Order.find();
+//   return orders;
+// }
+
+// export const obterPedidos = async () => {
+//   const authToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InBlZGlkb3NAdGVzdGUuY29tIiwiaWF0IjoxNjg0MTU4MzkyLCJleHAiOjE2ODQxNjE5OTIsInN1YiI6IjEwIn0.Ha9tqRmAHucCqwVvlZT2wrzYEutR6tO3BucwU5MmlYQ';
+//   try {
+//     const response = await fetch(`${API_URL}/orders`, {
+//       method: 'GET',
+//       headers: {
+//         'Content-Type': 'application/json',
+//         'Authorization': `Bearer ${authToken}`,
+//       },
+//     });
+//     const data = await response.json();
+//     const orders = await getOrders(); // chamada da função getOrders
+//     return { ...data, orders };
+//   } catch (error) {
+//     console.error(error);
+//     throw error;
+//   }
+// };
+
+
+
+  const API_URL = 'http://localhost:8080';
 
 export const obterPedidos = async () => {
-  const authToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InByb2RzZHNkdW9AdGVzdGUuY29tIiwiaWF0IjoxNjg0MDk1NDM3LCJleHAiOjE2ODQwOTkwMzcsInN1YiI6IjMifQ.41ZSV6SCMS881yaiHzrlW5VPa8hQ8Y4hAmkdB5Tpbmo';
-  try {
-    const response = await fetch(`${API_URL}/orders`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${authToken}`,
-      },
-    });
-    const data = await response.json();
-    const orders = await getOrders(); // chamada da função getOrders
-    return { ...data, orders };
-  } catch (error) {
-    console.error(error);
-    throw error;
+  const authToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InBlZHNzaWRveGNzQHRlc3RlLmNvbSIsImlhdCI6MTY4NDE2Njg2NCwiZXhwIjoxNjg0MTcwNDY0LCJzdWIiOiIxMiJ9.wuVRyGt7YrW5j2zDzCsXzClE2cmzpNtfr6R7oU5bnP4';
+  const response = await fetch(`${API_URL}/orders`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${authToken}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error('Erro ao obter pedidos');
   }
+
+  return response.json();
 };
---
+++ src/componentes/Pedidos/Pedidos.js
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from 'react';
+import { obterPedidos } from '../../API/Pedidos';
+import './Pedidos.css'
+
+const ListaPedidos = () => {
+  const [pedidos, setPedidos] = useState([]);
+
+  useEffect(() => {
+    carregarPedidos();
+  }, []);
+
+  const carregarPedidos = async () => {
+    try {
+      const pedidos = await obterPedidos();
+      setPedidos(pedidos);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className='lista-de-pedidos'>
+  <h2 className='titulo-lista-pedidos'>Lista de Pedidos</h2>
+  <table>
+    <thead>
+      <tr>
+        <th>ID do Pedido:</th>
+        <th>Cliente:</th>
+        <th>Mesa:</th>
+        <th>Status:</th>
+        <th>Data de Entrada:</th>
+        <th>Produto:</th>
+        <th>Quantidade:</th>
+      </tr>
+    </thead>
+    <tbody>
+      {pedidos.map((pedido) => (
+        <tr key={pedido.id}>
+          <td>{pedido.id}</td>
+          <td>{pedido.client}</td>
+          <td>{pedido.products[0].product.table}</td>
+          <td>{pedido.status}</td>
+          <td>{pedido.dateEntry}</td>
+          <td>
+            <ul>
+              {pedido.products.map((item) => (
+                <li key={item.product.id}>
+                  {item.product.name}
+                </li>
+              ))}
+            </ul>
+          </td>
+          <td>
+            <ul>
+              {pedido.products.map((item) => (
+                <li key={item.product.id}>
+                  {item.qty}
+                </li>
+              ))}
+            </ul>
+          </td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+</div>
+
+
+  );
+};
+
+export default ListaPedidos;
