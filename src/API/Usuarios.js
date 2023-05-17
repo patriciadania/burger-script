aaab2ab08b7173dd20805b3e969eed828e4e92a5
Thainara Tabile <thainara.tabilebenevides@gmail.com>
2023-05-17 10:23:06 -0300

    refatoração lista de pedidos Co-authored-by: patriciadania patricia.adania@hotmail.com

--- src/API/Usuarios.js
+++ src/API/Usuarios.js
@@ -1,6 +1,5 @@
-const API_URL = 'http://ec2-52-23-242-101.compute-1.amazonaws.com:8080';
-const AUTH_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFuaXRhLmJvcmdAc3lzdGVycy54eXoiLCJpYXQiOjE2ODQxOTcxMTAsImV4cCI6MTY4NDIwMDcxMCwic3ViIjoiMSJ9.x3ncrPSuFjzlNqDJVgvI5uanqjPhd7OGiunI8IYVh5w'
-
+const API_URL = 'http://localhost:8080';
+const AUTH_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFuaXRhLmJvcmdAc3lzdGVycy54eXoiLCJpYXQiOjE2ODQzMjkwMjYsImV4cCI6MTY4NDMzMjYyNiwic3ViIjoiMSJ9.Tp2_2nlZeuCP0S4WP-CTQfuf2dzbEHGSfLQa1v55hSw';
 
 export const login = async (email, password) => {
     const response = await fetch(`${API_URL}/login`, {
@@ -36,9 +35,7 @@ export const criarUsuario = async (nome, email, password, role) => {
 }
 
 export const listarUsuarios = async () => {
-    // const API_URL = 'http://ec2-52-23-242-101.compute-1.amazonaws.com:8080';
-    // const authToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InBhdHJpQHRlc3RlLmNvbSIsImlhdCI6MTY4NDEwMzA1NSwiZXhwIjoxNjg0MTA2NjU1LCJzdWIiOiI5In0.7hVuQEHVTDN1nbRqkkFF0icnfMT1EwwApfuYJwZx1JI'
-  
+   
     const response = await fetch(`${API_URL}/users`, {
       method: "GET",
       headers: {
@@ -53,31 +50,8 @@ export const listarUsuarios = async () => {
   
     return response.json();
   };
-
-
-//   export const obterNomeUsuario = async () => {
-//     const authToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFuYUB0ZXN0ZS5jb20iLCJpYXQiOjE2ODQwMDc1NDMsImV4cCI6MTY4NDAxMTE0Mywic3ViIjoiMzEifQ.N5uoBg5PwQTx_Rc7gPZguQisaoI2RB0BD4qTfv2QGRQ';
-  
-//     const response = await fetch(`${API_URL}/users`, {
-//       method: 'GET',
-//       headers: {
-//         'Content-Type': 'application/json',
-//         'Authorization': `Bearer ${authToken}`,
-//       },
-//     });
-  
-//     if (!response.ok) {
-//       throw new Error('Erro ao obter o nome do usuário');
-//     }
-  
-//     const usuario = await response.json();
-//     console.log(usuario)
-//     return usuario.nome;
-//   };
   
 export const deletarUsuario = async (id) => {
-    // const API_URL = 'http://localhost:8080';
-    // const authToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InBhdHJpQHRlc3RlLmNvbSIsImlhdCI6MTY4NDEwMzA1NSwiZXhwIjoxNjg0MTA2NjU1LCJzdWIiOiI5In0.7hVuQEHVTDN1nbRqkkFF0icnfMT1EwwApfuYJwZx1JI'
     const response = await fetch(`${API_URL}/users/${id}`, {
       method: 'DELETE',
       headers: {
@@ -93,7 +67,7 @@ export const deletarUsuario = async (id) => {
   
 
   export const editarUsuario = async (uid, novoUsuario) => {
-    // const authToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InBhdHJpQHRlc3RlLmNvbSIsImlhdCI6MTY4NDEwMzA1NSwiZXhwIjoxNjg0MTA2NjU1LCJzdWIiOiI5In0.7hVuQEHVTDN1nbRqkkFF0icnfMT1EwwApfuYJwZx1JI'
+  
     const response = await fetch(`${API_URL}/users/${uid}`, {
       method: "PATCH",
       headers: {
--- src/Paginas/Adm/PagProdutos/ListaDeProdutos.js
+++ src/Paginas/Adm/PagProdutos/ListaDeProdutos.js
@@ -2,71 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { obterProdutos } from '../../../API/Produtos';
 import './Produto.css'
 
-
-
-// const ListaDeProdutos = () => {
-//   const [produtos, setProdutos] = useState([]);
-
-//   useEffect(() => {
-//     const buscarProdutos = async () => {
-//       try {
-//         const produtosData = await obterProdutos();
-//         setProdutos(produtosData);
-//       } catch (error) {
-//         console.error(error);
-//       }
-//     };
-
-//     buscarProdutos();
-//   }, []);
-
-//   return (
-//     <div>
-//       <h1>Lista de Produtos</h1>
-//       <ul>
-//         {Array.isArray(produtos) && produtos.length > 0 ? (
-//           produtos.map((produto) => (
-            // <table key={produto.id}>
-            //     <thead>
-            //             <tr>
-            //                 <th>Nome do Produto:</th>
-            //                 <th>Valor R$</th>
-            //                 <th>Categoria</th>
-            //                 <th>Tipo de Refeição</th>
-            //             </tr>
-            //         </thead>
-            //         <tbody>
-            //             <tr>
-            //             <td>{produto.name}</td>
-            //             </tr>
-            //             <tr>
-            //             <td>R$ {produto.price}</td>
-            //             </tr>
-            //             <tr>
-            //             <td>{produto.title}</td>
-            //             </tr>
-            //             <tr>
-            //             <td>{produto.type}</td>
-            //             </tr>
-                       
-            //         </tbody>
-            // </table>
-//             <li key={produto.id}>
-//               <span>{produto.name}</span> -
-//                <span>R$ {produto.price}</span> 
-//                <span>{produto.title}</span> 
-//                <span>{produto.type}</span>
-//             </li>
-//           ))
-//         ) : (
-//           <li>Nenhum produto encontrado</li>
-//         )}
-//       </ul>
-//     </div>
-//   );
-// };
-
-// export default ListaDeProdutos;
 const ListaDeProdutos = () => {
     const [produtos, setProdutos] = useState([]);
   
@@ -102,7 +37,7 @@ const ListaDeProdutos = () => {
               <tr key={produto.id}>
                 <td className="nome-produto">{produto.name}</td>
                 <td className='valor-produto'>R$ {produto.price}</td>
-                <td className='categoria-produto'>{produto.title}</td>
+                <td className='categoria-produto'>{produto.category}</td>
                 <td className='tipo-refeicao'>{produto.type}</td>
                 <td className='tipo-refeicao'>{produto.id}</td>
               </tr>
