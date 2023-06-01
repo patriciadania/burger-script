5a50f8219cb1c4fab1b7d8ed650383b28de235cd
Thainara Tabile <thainara.tabilebenevides@gmail.com>
2023-06-01 13:36:32 -0300

    refatora lista de produtos admin  Co-authored-by: patriciadania patricia.adania@hotmail.com

--- src/Paginas/Adm/PagProdutos/ListaDeProdutos.js
+++ src/Paginas/Adm/PagProdutos/ListaDeProdutos.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { obterProdutos } from '../../../API/Produtos';
 import './Produto.css';
 
+
 const ListaDeProdutos = (props) => {
   const { tipoProduto } = props;
   const [produtos, setProdutos] = useState([]);
@@ -21,32 +22,29 @@ const ListaDeProdutos = (props) => {
 
   return (
     <div className='tabela-produtos'>
-      <h1 className='titulo-lista-produtos'>Lista de Produtos</h1>
-      <div className="loader"></div>
-      <table>
-        <thead>
-          <tr>
-            <th className="nome-produto">Nome do Produto:</th>
-            <th className='valor-produto'>Valor R$</th>
-            <th className='categoria-produto'>Categoria</th>
-            <th className='tipo-refeicao'>Tipo de Refeição</th>
-            <th className='tipo-refeicao'>Id:</th>
-          </tr>
-        </thead>
-        <tbody>
+      <h1 className='titulo-tabela-produtos'>Lista de Produtos</h1>
+      <dl className='tabela-produtos-admin'>
+      <div className='tabela-produtos-cabecalho'>
+            <dt className='tabela-produtos-id'>ID</dt>
+            <dt className='tabela-produtos-esquerda'>Nome do Produto</dt>
+            <dt className='tabela-produtos-direita'>Valor</dt>
+            <dt className='tabela-produtos-direita'>Quantidade</dt>
+            <dt>Ações</dt>
+            </div>
+       
           {produtos
             .filter((produto) => (!tipoProduto || produto.type === tipoProduto))
             .map((produto) => (
-              <tr key={produto.id}>
-                <td className="nome-produto">{produto.name}</td>
-                <td className='valor-produto'>R$ {produto.price}</td>
-                <td className='categoria-produto'>{produto.category}</td>
-                <td className='tipo-refeicao'>{produto.type}</td>
-                <td className='tipo-refeicao'>{produto.id}</td>
-              </tr>
+              <dd key={produto.id} className='tabela-produtos-itens'>
+                <span className='tabela-produtos-id'>{produto.id}</span>
+                <span className='tabela-produtos-esquerda'>{produto.name}</span>
+                <span className='tabela-produtos-direita'>R$ {produto.price}</span>
+                <span>{produto.qty}</span>
+                <span>edit/excl</span>
+              </dd>
             ))}
-        </tbody>
-      </table>
+       
+      </dl>
     </div>
   );
 };
