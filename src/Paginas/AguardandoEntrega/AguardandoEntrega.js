46e1f523b23da74861faad8dfe224eb5b2531e84
patriciadania <patricia.adania@hotmail.com>
2023-06-05 16:29:15 -0300

    Implementação do Modal. Co-authored-by: ThainaraTabile thainara.tabilebenevides@gmail.com

--- src/Paginas/AguardandoEntrega/AguardandoEntrega.js
+++ src/Paginas/AguardandoEntrega/AguardandoEntrega.js
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Modal from 'react-modal';
 import ListaPedidos from '../../componentes/Pedidos/Pedidos';
 import MenuNavegacao from '../../componentes/MenuNavegacao/MenuNavegacao';
 import { atualizarStatusPedido } from '../../API/Pedidos';
 import Botao from '../../componentes/Botao/Botao';
 
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    maxWidth: '400px',
+    width: '90%',
+    textAlign: 'center',
+  },
+};
+
+Modal.setAppElement('#root');
+
 export default function AguardandoEntrega() {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
   const marcarComoEntregue = async (pedido) => {
     try {
       await atualizarStatusPedido(pedido.id, 'pedido entregue');
-      alert('Pedido entregue com sucesso!');
-      window.location.reload();
+      setModalIsOpen(true);
     } catch (error) {
       console.error('Erro ao marcar pedido como entregue:', error);
     }
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+    window.location.reload();
   };
 
   return (
@@ -31,6 +54,15 @@ export default function AguardandoEntrega() {
           <Botao onClick={() => marcarComoEntregue(pedido)}>concluído</Botao>
         )}
       />
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Pedido Entregue"
+        style={customStyles} 
+      >
+        <h2>Pedido entregue com sucesso!</h2>
+        <Botao onClick={closeModal}>Fechar</Botao>
+      </Modal>
     </section>
   );
 }
--- src/Paginas/Cozinha/PedidosProntos.js
+++ src/Paginas/Cozinha/PedidosProntos.js
@@ -1,20 +1,43 @@
-import React from 'react';
+import React , { useState } from 'react';
 import { Link } from 'react-router-dom';
 import ListaPedidos from '../../componentes/Pedidos/Pedidos';
 import MenuNavegacao from '../../componentes/MenuNavegacao/MenuNavegacao';
 import Botao from '../../componentes/Botao/Botao';
 import { atualizarStatusPedido } from '../../API/Pedidos';
+import Modal from 'react-modal';
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    maxWidth: '400px',
+    width: '90%',
+    textAlign: 'center',
+  },
+};
+Modal.setAppElement('#root');
 
 export default function PedidosProntos() {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
   const enviarPedido = async (pedido) => {
     try {
       await atualizarStatusPedido(pedido.id, 'enviado');
-      alert('Pedido enviado com sucesso!');
-      window.location.reload();
+      setModalIsOpen(true);
+    
     } catch (error) {
       console.error('Erro ao enviar pedido:', error);
     }
   };
+  const closeModal = () => {
+    setModalIsOpen(false);
+ 
+    window.location.reload();
+  };
+
 
   return (
     <section className="telaFazerPedido">
@@ -31,6 +54,15 @@ export default function PedidosProntos() {
           <Botao onClick={() => enviarPedido(pedido)}>enviar</Botao>
         )}
       />
+       <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Pedido Entregue"
+        style={customStyles} 
+      >
+        <h2>Pedido enviado com sucesso!</h2>
+        <Botao onClick={closeModal}>Fechar</Botao>
+      </Modal>
     </section>
   );
 }
